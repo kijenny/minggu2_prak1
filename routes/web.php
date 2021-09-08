fe912ace14569e7feeea08cad4361cb058fbcc5a
kijenny <kiranajenny46@gmail.com>
2021-09-08 12:47:31 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    echo('selamat datang');
+});
+
+Route::get('/about', function () {
+    echo('Kirana Jenny Alqorni');
+    echo('2031710086');
+});
+
+Route::get('/articles/1', function () {
+    echo('halaman artikel dengan id 1');
+});
+
+Route::get('/articles/2', function () {
+    echo('halaman artikel dengan id 2');
+});
